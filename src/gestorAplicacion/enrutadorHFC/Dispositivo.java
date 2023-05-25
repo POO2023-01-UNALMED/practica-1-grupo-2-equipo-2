86e3d6a39a0dddcdd921a98120fa903b91ddcd52
jcmisas <109988112+jcmisas@users.noreply.github.com>
2023-05-24 20:26:34 -0500

    class Dispositivos's commit

--- src/gestorAplicacion/enrutadorHFC/Dispositivo.java
+++ src/gestorAplicacion/enrutadorHFC/Dispositivo.java
@@ -1,96 +1,87 @@
 package gestorAplicacion.enrutadorHFC;
-import java.util.*;
 
 import gestorAplicacion.host.Cliente;
 
-public class Dispositivo{
+import java.io.Serializable;
+import java.util.ArrayList;
 
-	// Atributos
+public class Dispositivo implements Serializable{
+
+	//CLASE IMPORTANTE PARA LA FUNCIONALIDAD DE VISUALIZAR DISPOSITIVOS, SIN EMBARGO TAMBIÉN TIENE IMPLICACIONES EN OTRAS EN PROCESOS SECUNDARIOS (TEST--REPORTE--MEJORA TU PLAN)
+
+	//ATRIBUTOS
 	private Router modem;
 	private String ipAsociada;
 	private String nombre;
-  private String generacion;
+  	private String generacion;
 	private static ArrayList<Dispositivo> dispositivosTotales = new ArrayList<>();
   
 
-	  // Constructores
-	  public Dispositivo(Router modem,String nombre,String generacion) {
+	//CONSTRUCTORS
+	public Dispositivo(Router modem,String nombre,String generacion) {
 	    this.modem = modem;
 	    this.nombre = nombre;
-	    this.ipAsociada=this.modem.getIP();
-      this.generacion=generacion;
-      Dispositivo.dispositivosTotales.add(this);
-	  }
+	    ipAsociada=this.modem.getIP();
+      	this.generacion=generacion;
+      	dispositivosTotales.add(this);
+	}
 
     //MÉTODOS
 
-    //Funcionalidad Test()
-	  public String desconectarDispositivo(Cliente cliente,Dispositivo dispositivo) {
+    //METODO DE ESTATICO---FUNCIONALIDAD TEST
+	public static String desconectarDispositivo(Cliente cliente,Dispositivo dispositivo) {
 	    if(cliente.getModem().verificarDispositivos(dispositivosTotales,cliente.getModem()).contains(dispositivo)){
 	      dispositivo.setIpAsociada("");
-	      return "Desconectado correctamente.";
-
-	      
+	      return "Dispositivo desconectado correctamente.";
 	    }else{
 	      return "Dispositivo inválido, intente de nuevo.";
 	    } 
-	  }
+	}
 
-    //Funcionalidad Test()
-	  public String conectarDispositivo(Cliente cliente,Dispositivo dispositivo){
-	    ArrayList<Dispositivo> d_conectados= cliente.getModem().verificarDispositivos(dispositivosTotales, cliente.getModem());
-	    if(d_conectados.contains(dispositivo)){
+	//METODO TOSTRING
+	public String toString() {
+		return "Dispositivo: "+ this.nombre + "\nDirección IP: "+ this.ipAsociada+"\nGeneración: "+this.generacion;
+	}
 
-	      return "El dispositivo ya está conectado.";
-	    }else{
-	      
-	      dispositivo.setIpAsociada(cliente.getModem().getIP());
-	      return "Dispositivo conectado.";
-	    }  
-	  }
 
-  public String toString() {
-    return "Dispositivo: "+this.nombre+"\nDirección IP: "+this.ipAsociada+"\nGeneración: "+this.generacion;
-  }
-  // Setters y Getters
-  public Router getModem() {
-  	return modem;
-  }
-  
-  public void setModem(Router modem) {
-  	this.modem = modem;
-  }
-  
-  public String getIpAsociada() {
-  	return ipAsociada;
-  }
-  
-  public void setIpAsociada(String ipAsociada) {
-  	this.ipAsociada = ipAsociada;
-  }
-  
-  public String getNombre() {
-  	return nombre;
-  }
-  
-  public void setNombre(String nombre) {
-  	this.nombre = nombre;
-  }
-  
-  public String getGeneracion() {
-  	return generacion;
-  }
-  
-  public void setGeneracion(String generacion) {
-  	this.generacion = generacion;
-  }
-  
-  public static ArrayList<Dispositivo> getDispositivosTotales() {
-  	return dispositivosTotales;
-  }
-  
-  public static void setDispositivosTotales(ArrayList<Dispositivo> dispositivosTotales) {
-  	Dispositivo.dispositivosTotales = dispositivosTotales;
-  }
-  
+	//GETTERS Y SETTERS
+	public Router getModem() {
+		return modem;
+	}
+
+	public void setModem(Router modem) {
+		this.modem = modem;
+	}
+
+	public String getIpAsociada() {
+		return ipAsociada;
+	}
+
+	public void setIpAsociada(String ipAsociada) {
+		this.ipAsociada = ipAsociada;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGeneracion() {
+		return generacion;
+	}
+
+	public void setGeneracion(String generacion) {
+		this.generacion = generacion;
+	}
+
+	public static ArrayList<Dispositivo> getDispositivosTotales() {
+		return dispositivosTotales;
+	}
+
+	public static void setDispositivosTotales(ArrayList<Dispositivo> dispositivosTotales) {
+		Dispositivo.dispositivosTotales = dispositivosTotales;
+	}
 }
